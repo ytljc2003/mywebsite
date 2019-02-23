a3c553ef36f29e64fc8fcea920065c2e9ec4e621
ytljc2003 <ytljc2003@gmail.com>
2019-02-22 19:39:19 -0600

    add some template

--- branch.cpp
+++ branch.cpp
@@ -1,8 +1,21 @@
 #include <stream>
 
+template <typename T>
+struct person{
+int age;
+char *name;
+T something;
+};
+
 int main()
 {
+	person<int> x;
+	x.something = 1;
+
+	person<char *> y;
+	y.something = "hoge";
+
 	//Here add something
-	stdout << "test cplusplus" << endl;
+	std::out << "test cplusplus" << endl;
 	return 0;
 }
